e7f5e02211ae6a283aec1894ab9299b258bb6db2
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-03-05 10:53:20 +0530

    Add files via upload

---
+++ src/test/java/mypackage/CalculatorTest.java
@@ -0,0 +1,58 @@
+// /*
+package mypackage;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void twoAndThreeIsFive() throws Exception {
+        final long result = new Calculator().addFucn(2, 3);
+        assertThat(result, is(5L));
+        //assertThat(result, is(6L));
+    }
+
+    @Test
+    public void threeMinusTwoIsOne() throws Exception {
+        final long result = new Calculator().subFucn(2, 3);
+        assertThat(result, is(1L));
+        // assertThat(result, is(2L));
+    }
+    
+    @Test
+    public void threeXThreeIsNine() throws Exception {
+        final long result = new Calculator().mulFucn(3, 3);
+        assertThat(result, is(9L));
+        //assertThat(result, is(8L));
+    }
+}
+// */
+// ----sonar-----
+/*
+package mypackage;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void twoAndThreeIsFive() throws Exception {
+        final long result = new Calculator().addFucn(2, 3);
+        assertThat(result, is(5L));
+    }
+
+    @Test
+    public void threeMinusTwoIsOne() throws Exception {
+        final long result = new Calculator().subFunc(2, 3); // Corrected the method name
+        assertThat(result, is(1L));
+    }
+    
+    @Test
+    public void threeXThreeIsNine() throws Exception {
+        final long result = new Calculator().mulFucn(3, 3);
+        assertThat(result, is(9L));
+    }
+}
+// */
