30dcd4a4778602210a636f4ed528fb7dcbe78cd1
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-03-07 11:24:52 +0530

    Update CalculatorTest.java

--- src/test/java/mypackage/CalculatorTest.java
+++ src/test/java/mypackage/CalculatorTest.java
@@ -9,17 +9,20 @@ public class CalculatorTest {
     public void twoAndThreeIsFive() throws Exception {
         final long result = new Calculator().addFucn(2, 3);
         assertThat(result, is(5L));
+        //assertThat(result, is(6L));
     }
 
     @Test
     public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subFunc(2, 3); // Corrected the method name
+        final long result = new Calculator().subFucn(2, 3);
         assertThat(result, is(1L));
+        // assertThat(result, is(2L));
     }
     
     @Test
     public void threeXThreeIsNine() throws Exception {
         final long result = new Calculator().mulFucn(3, 3);
         assertThat(result, is(9L));
+        //assertThat(result, is(8L));
     }
 }
